53e2fde2fef1a2ef23dfb651542bc8733748a7ac
Pedro Henrique Gonçalves <pedro_developer@outlook.com>
2024-12-08 17:23:45 -0300

    NavBar added with icon menu

---
+++ src/components/navigation/navBar/index.tsx
@@ -0,0 +1,11 @@
+import { SideBarMobile } from "../sideBar-mobile";
+
+const NavBar = () => {
+  return (
+    <header className="h-12 bg-[#f0ddb5] flex items-center">
+      <SideBarMobile />
+    </header>
+  );
+};
+
+export default NavBar;
