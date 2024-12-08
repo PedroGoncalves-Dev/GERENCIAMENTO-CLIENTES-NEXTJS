364d814465bf2bbf6db041d4ca5c7c92dae49149
Pedro Henrique Gonçalves <pedro_developer@outlook.com>
2024-12-08 17:24:26 -0300

    sideBar components

---
+++ src/components/navigation/sideBar-mobile/index.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { RiMenu5Line } from "react-icons/ri";
+
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+export function SideBarMobile() {
+  return (
+    <div className="grid grid-cols-2 gap-2">
+      <Sheet>
+        <SheetTrigger asChild>
+          <RiMenu5Line size={25} className="ml-2" />
+        </SheetTrigger>
+        <SheetContent side={"left"}>
+          <SheetHeader>
+            <SheetTitle>Dashboard</SheetTitle>
+            <SheetDescription></SheetDescription>
+          </SheetHeader>
+
+          <SheetFooter>
+            <SheetClose asChild>
+              <Button type="submit">Save changes</Button>
+            </SheetClose>
+          </SheetFooter>
+        </SheetContent>
+      </Sheet>
+    </div>
+  );
+}
