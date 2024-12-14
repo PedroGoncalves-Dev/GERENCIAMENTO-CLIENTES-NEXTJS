b48c51db36ad377a0cd3f135c6b2ef99e947367e
Pedro Henrique Gonçalves <pedro_developer@outlook.com>
2024-12-14 13:49:43 -0300

    chart most sales end chart spent more added to home

--- src/app/(system)/home/page.tsx
+++ src/app/(system)/home/page.tsx
@@ -1,3 +1,5 @@
+import { ChartMostSales } from "@/components/charts/moreBought";
+import { ChartSpentMore } from "@/components/charts/spentMore";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -7,8 +9,22 @@ export const metadata: Metadata = {
 
 export default function home() {
   return (
-    <main>
-      <h1>Home</h1>
+    <main className="w-full py-9">
+      <section className="bg-white w-[90%] mx-auto rounded-md">
+        <h1 className="text-3xl font-bold text-blue-900 text-center mb-4">
+          {" "}
+          Dashboard
+        </h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div>
+            <ChartMostSales />
+          </div>
+
+          <div>
+            <ChartSpentMore />
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
