6407aa21b49f56db2d4659841fe5e935c1fcce89
Pedro Henrique Gonçalves <pedro_developer@outlook.com>
2024-12-14 13:14:08 -0300

    Added logo

--- src/components/navigation/navBar/index.tsx
+++ src/components/navigation/navBar/index.tsx
@@ -1,8 +1,16 @@
+import Image from "next/image";
 import { SideBarMobile } from "../sideBar-mobile";
 
 const NavBar = () => {
   return (
     <header className="h-12 bg-[#f0ddb5] flex items-center">
+      <Image
+        src="/icons/logo.svg"
+        alt="logo"
+        width={80}
+        height={50}
+        className="ml-2"
+      />
       <SideBarMobile />
     </header>
   );
