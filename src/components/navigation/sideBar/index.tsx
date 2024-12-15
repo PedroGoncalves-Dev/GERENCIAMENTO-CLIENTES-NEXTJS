ee8e868525081b1cd5013ff12f126219fd37ab60
Pedro Henrique Gonçalves <pedro_developer@outlook.com>
2024-12-15 14:30:37 -0300

    component structured for mobile

--- src/components/navigation/sideBar/index.tsx
+++ src/components/navigation/sideBar/index.tsx
@@ -1,11 +1,30 @@
+import { Separator } from "@/components/ui/separator";
+import { FaUserTag } from "react-icons/fa6";
+import Link from "next/link";
+
 const SideBar = () => {
   return (
-    <aside>
+    <aside className="h-full px-4">
       <nav>
         <ul>
-          <li>Item 1</li>
-          <li>Item 2</li>
-          <li>Item 3</li>
+          <Link href={"/"} className="flex items-center gap-3">
+            <FaUserTag />
+            <li className="font-semibold py-2">Gestão de Clientes</li>
+          </Link>
+
+          <Separator />
+
+          <Link href={"/"}>
+            <li className="font-semibold py-2">Item 2</li>
+          </Link>
+
+          <Separator />
+
+          <Link href={"/"}>
+            <li className="font-semibold py-2">Item 3</li>
+          </Link>
+
+          <Separator />
         </ul>
       </nav>
     </aside>
