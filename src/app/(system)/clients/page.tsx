37a0b92b3f27f1e44308771db4d253fbeeec879d
Pedro Henrique Gonçalves <pedro_developer@outlook.com>
2024-12-15 19:48:39 -0300

    page customers structured with title and button inatived

--- src/app/(system)/clients/page.tsx
+++ src/app/(system)/clients/page.tsx
@@ -2,6 +2,8 @@ import { DataTable } from "@/components/table/clients/data-table";
 import { columns } from "@/components/table/clients/columns";
 import { getAllClients } from "@/data-access/clients/get-all";
 import type { Metadata } from "next";
+import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
 
 export const metadata: Metadata = {
   title: "Clientes",
@@ -9,10 +11,18 @@ export const metadata: Metadata = {
 };
 
 export default async function clients() {
-  // const data = await getAllClients();
+  const data = await getAllClients();
   return (
-    <main>
-      <DataTable columns={columns} data={[]} />
+    <main className="bg-white w-[90%] mx-auto rounded-md p-5 my-4 ">
+      <section className="flex items-center justify-between">
+        <h1 className="text-3xl font-semibold">Tabela de clientes</h1>
+
+        <div>
+          <Button>Ativos</Button>
+        </div>
+      </section>
+      <Separator className="my-4" />
+      <DataTable columns={columns} data={data} />
     </main>
   );
 }
