6cfa9a37b37eccabf4b1c04f8f080ecd51893a36
Pedro Henrique Gonçalves <pedro_developer@outlook.com>
2024-12-15 14:50:28 -0300

    added dashboard to sideBar

--- src/components/navigation/sideBar/index.tsx
+++ src/components/navigation/sideBar/index.tsx
@@ -1,12 +1,22 @@
 import { Separator } from "@/components/ui/separator";
 import { FaUserTag } from "react-icons/fa6";
+import { FaHome } from "react-icons/fa";
+
 import Link from "next/link";
 
 const SideBar = () => {
   return (
-    <aside className="h-full px-4">
+    <aside className="h-full px-4 bg-orange-50 lg:h-screen">
       <nav>
         <ul>
+          <Link href={"/"} className="flex items-center justify-center gap-3">
+            <FaHome />
+
+            <li className="font-semibold py-8 text-center">Dashboard</li>
+          </Link>
+
+          <Separator />
+
           <Link href={"/"} className="flex items-center gap-3">
             <FaUserTag />
             <li className="font-semibold py-2">Gestão de Clientes</li>
