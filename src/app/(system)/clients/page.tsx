ecf23eae64657fcadb9e42ff6d6825386cce0e6a
Pedro Henrique Gonçalves <pedro_developer@outlook.com>
2024-12-16 00:02:48 -0300

    added component switch

--- src/app/(system)/clients/page.tsx
+++ src/app/(system)/clients/page.tsx
@@ -3,7 +3,7 @@ import { columns } from "@/components/table/clients/columns";
 import { getAllClients } from "@/data-access/clients/get-all";
 import type { Metadata } from "next";
 import { Separator } from "@/components/ui/separator";
-import { Button } from "@/components/ui/button";
+import SwitchInativosAtivos from "./_components/switch-inativos-ativos";
 
 export const metadata: Metadata = {
   title: "Clientes",
@@ -14,12 +14,10 @@ export default async function clients() {
   const data = await getAllClients();
   return (
     <main className="bg-white w-[90%] mx-auto rounded-md p-5 my-4 ">
-      <section className="flex items-center justify-between">
+      <section className="flex items-center justify-between flex-col gap-4 lg:flex-row lg:gap-0 ">
         <h1 className="text-3xl font-semibold">Tabela de clientes</h1>
 
-        <div>
-          <Button>Ativos</Button>
-        </div>
+        <SwitchInativosAtivos />
       </section>
       <Separator className="my-4" />
       <DataTable columns={columns} data={data} />
