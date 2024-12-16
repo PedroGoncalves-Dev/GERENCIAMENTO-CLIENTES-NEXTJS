731e8413b760f91f7256ccf89660f633a281c3af
Pedro Henrique Gonçalves <pedro_developer@outlook.com>
2024-12-16 00:01:46 -0300

    page addClient added

---
+++ src/app/(system)/addedClient/page.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import FormNewClient from "./_components/form-new-client";
+import { Separator } from "@/components/ui/separator";
+
+export const metadata: Metadata = {
+  title: "Adicionar Cliente",
+  description: "Adicionar novo cliente",
+};
+
+export default function addedClient() {
+  return (
+    <main className="bg-white w-[90%] mx-auto rounded-md p-5 my-4 ">
+      <div>
+        <h1 className="font-bold text-center">Adicionar novo cliente</h1>
+      </div>
+
+      <Separator className="my-4" />
+
+      <FormNewClient />
+    </main>
+  );
+}
