4d57614dcb1b9b11fe8de795021cbe967926e26e
Pedro Henrique Gonçalves <pedro_developer@outlook.com>
2024-12-18 20:04:16 -0300

    form pra editar estruturado

--- src/components/table/clients/_components/editClient.tsx
+++ src/components/table/clients/_components/editClient.tsx
@@ -7,11 +7,17 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog";
 import { Iclients } from "@/data-access/clients/get-all";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { newClientSchema, TypeNewClient } from "@/schema/clients/schema-create";
 
 interface IpropsModalEdit {
   cliente: Iclients;
 }
 const EditClient = ({ cliente }: IpropsModalEdit) => {
+  const { register, handleSubmit } = useForm<TypeNewClient>({
+    resolver: zodResolver(newClientSchema),
+  });
   return (
     <DialogContent>
       <DialogHeader>
@@ -33,6 +39,29 @@ const EditClient = ({ cliente }: IpropsModalEdit) => {
                 defaultValue={cliente.email_cli}
                 className="mt-1 block w-full rounded-md border border-input bg-background px-3 py-2"
               />
+            </div>
+
+            <div>
+              <label htmlFor="" className="block text-sm font-medium">
+                Data de nascimento
+              </label>
+              <input type="text" />
+            </div>
+
+            <div className="grid grid-cols-2 gap-4">
+              <div>
+                <label htmlFor="" className="block text-sm font-medium">
+                  CPF:
+                </label>
+                <input type="text" className="border-b" />
+              </div>
+
+              <div>
+                <label htmlFor="" className="block text-sm font-medium">
+                  Telefone:
+                </label>
+                <input type="text" className="border-b" />
+              </div>
             </div>
           </form>
         </DialogDescription>
