4f375a97b540eea49cb36d1c2a1723648a0c2a60
Pedro Henrique Gonçalves <pedro_developer@outlook.com>
2024-12-18 00:57:28 -0300

    condition to show edit or details

--- src/components/table/clients/_components/dropdown.tsx
+++ src/components/table/clients/_components/dropdown.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useState } from "react";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -6,15 +8,31 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { useToast } from "@/hooks/use-toast";
-import { FaCopy, FaRegEdit, FaSortAmountDown } from "react-icons/fa";
+import { FaRegCopy, FaRegEdit, FaSortAmountDown } from "react-icons/fa";
 import { MdOutlineAutoDelete } from "react-icons/md";
 import { TbListDetails } from "react-icons/tb";
 import { FcOk } from "react-icons/fc";
 import { TiExportOutline } from "react-icons/ti";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import DetailsClient from "./detailsClient";
+import EditClient from "./editClient";
 
 const DropdownTable = ({ row }: { row: any }) => {
   const { toast } = useToast();
   const cliente = row.original;
+  const [isDetailsOpen, setIsDetailsOpen] = useState(false);
+  const [isEditOpen, setIsEditOpen] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
 
   const handleCopy = async () => {
     try {
@@ -27,7 +45,6 @@ const DropdownTable = ({ row }: { row: any }) => {
             ID copiado com sucesso!
           </div>
         ),
-
         duration: 3000,
       });
     } catch (error) {
@@ -39,33 +56,65 @@ const DropdownTable = ({ row }: { row: any }) => {
     }
   };
 
+  const handleDetailsClick = () => {
+    setIsDetailsOpen(true);
+    setDropdownOpen(false);
+    setModalOpen(true);
+  };
+
+  const handleEditClick = () => {
+    setIsEditOpen(true);
+    setDropdownOpen(false);
+    setModalOpen(true);
+  };
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger className="outline-none hover:text-slate-500 hover:cursor-pointer hover:rotate-90 transition-all duration-300 ease-in-out">
-        <FaSortAmountDown size={20} />
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem
-          className="cursor-pointer flex justify-between"
-          onClick={handleCopy}
-        >
-          Copiar ID <FaCopy />
-        </DropdownMenuItem>
-        <DropdownMenuItem className="cursor-pointer flex justify-between">
-          Detalhes <TbListDetails />
-        </DropdownMenuItem>
-        <DropdownMenuItem className="cursor-pointer flex justify-between">
-          Editar <FaRegEdit />
-        </DropdownMenuItem>
-        <DropdownMenuItem className="cursor-pointer flex justify-between">
-          Exportar dados <TiExportOutline />
-        </DropdownMenuItem>
-        <DropdownMenuItem className="cursor-pointer flex justify-between">
-          Inativar <MdOutlineAutoDelete />
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <>
+      <Dialog open={modalOpen} onOpenChange={setModalOpen}>
+        <DropdownMenu open={dropdownOpen} onOpenChange={setDropdownOpen}>
+          <DropdownMenuTrigger className="outline-none hover:text-slate-500 hover:cursor-pointer hover:rotate-90 transition-all duration-300 ease-in-out">
+            <FaSortAmountDown size={20} />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              className="cursor-pointer flex justify-between"
+              onClick={handleCopy}
+            >
+              Copiar ID <FaRegCopy />
+            </DropdownMenuItem>
+            <DialogTrigger asChild>
+              <DropdownMenuItem
+                className="cursor-pointer flex justify-between"
+                onClick={handleDetailsClick}
+              >
+                Detalhes <TbListDetails />
+              </DropdownMenuItem>
+            </DialogTrigger>
+
+            <DialogTrigger asChild>
+              <DropdownMenuItem
+                className="cursor-pointer flex justify-between"
+                onClick={handleEditClick}
+              >
+                Editar <FaRegEdit />
+              </DropdownMenuItem>
+            </DialogTrigger>
+            <DropdownMenuItem className="cursor-pointer flex justify-between">
+              Exportar dados <TiExportOutline />
+            </DropdownMenuItem>
+            <DropdownMenuItem className="cursor-pointer flex justify-between">
+              Inativar <MdOutlineAutoDelete />
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        {/* Modal de Detalhes */}
+
+        {isDetailsOpen && <DetailsClient cliente={cliente} />}
+
+        {isEditOpen && <EditClient cliente={cliente} />}
+      </Dialog>
+    </>
   );
 };
 
