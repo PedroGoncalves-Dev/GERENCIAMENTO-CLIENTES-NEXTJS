47d4cea54fca24beb13eeff351abbee67692b895
Pedro Henrique Gonçalves <pedro_developer@outlook.com>
2024-12-19 14:38:15 -0300

    função de excel

---
+++ src/components/excel/function-excel.ts
@@ -0,0 +1,15 @@
+import * as XLSX from "xlsx";
+
+export const exportToExcel = (data: any[], fileName: string) => {
+  // Cria uma nova planilha
+  const workbook = XLSX.utils.book_new();
+
+  // Converte os dados para o formato de planilha
+  const worksheet = XLSX.utils.json_to_sheet(data);
+
+  // Adiciona a planilha ao workbook
+  XLSX.utils.book_append_sheet(workbook, worksheet, "Dados");
+
+  // Gera e faz download do arquivo
+  XLSX.writeFile(workbook, `${fileName}.xlsx`);
+};
