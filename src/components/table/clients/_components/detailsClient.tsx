61a4a81237aed46a50b2a5e4b8c82cc8648589a7
Pedro Henrique Gonçalves <pedro_developer@outlook.com>
2024-12-25 17:41:39 -0300

    modal de detalhes de clientes estilizado

--- src/components/table/clients/_components/detailsClient.tsx
+++ src/components/table/clients/_components/detailsClient.tsx
@@ -9,6 +9,7 @@ import {
 } from "@/components/ui/dialog";
 
 import { Iclients } from "@/data-access/clients/get-all";
+import PhoneFormatter from "@/services/phoneFormated";
 
 interface IpropsModal {
   cliente: Iclients;
@@ -18,24 +19,82 @@ const DetailsClient = ({ cliente }: IpropsModal) => {
     <DialogContent>
       <DialogHeader>
         <DialogTitle>Detalhes do Cliente</DialogTitle>
-        <DialogDescription asChild>
-          <div className="space-y-4">
-            <div className="grid grid-cols-2 gap-2">
-              <span className="font-semibold">ID:</span>
-              <span>{cliente.id_cli}</span>
-
-              <span className="font-semibold">Nome:</span>
-              <span>{cliente.nome_cli}</span>
-
-              <span className="font-semibold">Email:</span>
-              <span>{cliente.email_cli}</span>
-
-              <span className="font-semibold">CPF:</span>
-              <span>{cliente.cpf_cli}</span>
-            </div>
-          </div>
-        </DialogDescription>
       </DialogHeader>
+      <div className="grid grid-cols-repeat-1-min400px lg:grid-cols-2 gap-4 ">
+        <div className="bg-blue-800 rounded-lg flex justify-end shadow-md">
+          <div className="grid grid-cols-2 gap-2 bg-slate-100 w-[97%] p-4">
+            <h2 className="col-span-2 text-lg font-bold border-b pb-2 text-blue-900">
+              Dados pessoais
+            </h2>
+            <span className="font-bold text-sm md:text-base">ID:</span>{" "}
+            <span className="text-sm md:text-base">{cliente.id_cli}</span>
+            <span className="font-bold text-sm md:text-base">Nome:</span>
+            <span className="text-sm md:text-base">{cliente.nome_cli}</span>
+            <span className="font-bold text-sm md:text-base">CPF:</span>
+            <PhoneFormatter type="cpf" value={cliente.cpf_cli} />
+            <span className="font-bold text-sm md:text-base">
+              Data de nascimento:
+            </span>
+            <span className="text-sm md:text-base">
+              {cliente.data_nascimento_cli}
+            </span>
+          </div>
+        </div>
+
+        <div className="bg-blue-800 rounded-lg flex justify-end shadow-md">
+          <div className="grid grid-cols-2 gap-2 bg-slate-100 w-[97%] p-4">
+            <h2 className="col-span-2 text-lg font-bold border-b pb-2 text-blue-900">
+              Endereço
+            </h2>
+            <span className="font-bold text-sm md:text-base">CEP:</span>{" "}
+            <span className="text-sm md:text-base">{cliente.cep}</span>
+            <span className="font-bold text-sm md:text-base">Logradouro:</span>
+            <span className="text-sm md:text-base">{cliente.logradouro}</span>
+            <span className="font-bold text-sm md:text-base">Complemento:</span>
+            <span className="text-sm md:text-base">{cliente.complemento}</span>
+            <span className="font-bold text-sm md:text-base">Bairro:</span>
+            <span className="text-sm md:text-base">{cliente.bairro}</span>
+            <span className="font-bold text-sm md:text-base">Cidade:</span>
+            <span className="text-sm md:text-base">{cliente.cidade}</span>
+            <span className="font-bold text-sm md:text-base">UF:</span>{" "}
+            <span className="text-sm md:text-base">{cliente.estado}</span>
+          </div>
+        </div>
+
+        <div className="bg-blue-800 rounded-lg flex justify-end shadow-md">
+          <div className="grid grid-cols-2 gap-2 bg-slate-100 w-[97%] p-4">
+            <h2 className="col-span-2 text-lg font-bold border-b pb-2 text-blue-900">
+              Contato
+            </h2>
+            <span className="font-bold text-sm md:text-base">Telefone: </span>
+            <PhoneFormatter value={cliente.telefone_cli} type="celular" />
+            <span className="font-bold text-sm md:text-base">email: </span>
+            <span className="text-sm md:text-base">{cliente.email_cli}</span>
+          </div>
+        </div>
+
+        <div className="bg-blue-800 rounded-lg flex justify-end shadow-md">
+          <div className="grid grid-cols-2 gap-2 bg-slate-100 w-[97%] p-4">
+            <h2 className="col-span-2 text-lg font-bold border-b pb-2 text-blue-900">
+              Observações:
+            </h2>
+            <span className="font-bold text-sm md:text-base">
+              Cadastrado criado:
+            </span>{" "}
+            <span className="text-sm md:text-base">{cliente.criado_em}</span>
+            <span className="font-bold text-sm md:text-base">
+              Atualizado em:
+            </span>{" "}
+            <span className="text-sm md:text-base">
+              {cliente.atualizado_em}
+            </span>
+            <span className="font-bold text-sm md:text-base">Status:</span>
+            <span className="text-sm md:text-base">
+              {cliente.status ? "Ativo" : "Inativo"}
+            </span>
+          </div>
+        </div>
+      </div>
       <DialogFooter>
         <DialogClose asChild>
           <Button>Fechar</Button>
